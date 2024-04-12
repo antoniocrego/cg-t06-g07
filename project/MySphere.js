b761d1b7c616166f0a475c0f6a1072e26fc65e75
Daniel Bernardo <up202108667@edu.fe.up.pt>
2024-04-12 10:47:58 +0100

    proj-1

--- project/MySphere.js
+++ project/MySphere.js
@@ -21,33 +21,33 @@ export class MySphere extends CGFobject {
             var alpha = i * delta_alpha;
             for (j = 0; j <= this.slices; j++) {
                 var beta = j * delta_beta;
-                var x = Math.sin(alpha) * Math.cos(beta);
-                var z = Math.sin(alpha) * Math.sin(beta);
+                var z = Math.sin(alpha) * Math.cos(beta);
+                var x = Math.sin(alpha) * Math.sin(beta);
                 var y = Math.cos(alpha);
 
                 this.vertices.push(x, y, z);
                 this.normals.push(x, y, z);
 
-                //this.texCoords.push(j / this.slices, i / (2*this.stacks));
+                this.texCoords.push(j / this.slices, i / (2*this.stacks));
             }
         }
 
         for (i = 0; i < 2*this.stacks; i++) {
             for (j = 0; j < this.slices; j++) {
                 if(i == 0){
-                    this.indices.push(j, j+(i+1)*this.slices,(j+1)+(i+1)*this.slices);
+                    this.indices.push(j, j+(i+1)*(this.slices+1),(j+1)+(i+1)*(this.slices+1));
                 }else if (i == 2*this.stacks - 1){
-                    this.indices.push(j + i*this.slices, (j+1) + (i+1)*this.slices, (j+1) + (i)*this.slices);
+                    this.indices.push(j + i*(this.slices+1), (j+1) + (i+1)*(this.slices+1), (j+1) + (i)*(this.slices+1));
                 }else{
-                    this.indices.push(j + i*this.slices, j+(i+1)*this.slices, (j+1) + (i)*this.slices);
-                    this.indices.push(j+(i+1)*this.slices,(j+1)+(i+1)*this.slices, (j+1)+(i)*this.slices);
+                    this.indices.push(j + i*(this.slices+1), j+(i+1)*(this.slices+1), (j+1) + (i)*(this.slices+1));
+                    this.indices.push(j+(i+1)*(this.slices+1),(j+1)+(i+1)*(this.slices+1), (j+1)+(i)*(this.slices+1));
                 }
             }
         }
 
         
 
-        this.primitiveType = this.scene.gl.LINES;
+        this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
 
     }
